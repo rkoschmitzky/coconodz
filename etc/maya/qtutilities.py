c3c04331ed54b4755d60e3fe431364f94d9e499e
rico <contact@ricokoschmitzky.com>
2017-06-16 09:24:25 +0200

    Added some maya qt utilities

--- etc/maya/qtutilities.py
+++ etc/maya/qtutilities.py
@@ -2,7 +2,32 @@ try:
     from shiboken import wrapInstance
 except:
     from shiboken2 import wrapInstance
+#todo add logging
+
+from Qt import QtWidgets
+
+import maya.OpenMayaUI as omui
 
 
 def wrapinstance(*args, **kwargs):
     return wrapInstance(*args, **kwargs)
+
+
+def maya_main_window():
+    """ Return the QMainWindow for the Maya main Window """
+
+    ptr = omui.MQtUtil.mainWindow()
+    if not ptr:
+        raise RuntimeError('No Maya window found.')
+    window = wrapinstance(long(ptr), QtWidgets.QMainWindow)
+    return window
+
+
+def maya_menu_bar():
+    """ Maya menubar for """
+
+    for child in maya_main_window().children():
+        if isinstance(child, QtWidgets.QMenuBar):
+            return child
+
+
