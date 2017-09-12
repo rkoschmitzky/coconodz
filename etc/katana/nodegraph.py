f4d5cb591e8f3e62c66d601e9e668b6596e4c162
rico <contact@ricokoschmitzky.com>
2017-09-12 08:16:46 +0200

    removed redundant katana statup code

--- etc/katana/nodegraph.py
+++ etc/katana/nodegraph.py
@@ -1,6 +1,7 @@
 import coconodz.nodegraph as nodegraph
 from coconodz.etc.katana.qtutilities import get_katana_main_window
 
+
 class Nodzgraph(nodegraph.Nodegraph):
 
     def __init__(self, parent=get_katana_main_window()):
--- etc/katana/startup/init.py
+++ etc/katana/startup/init.py
@@ -11,7 +11,3 @@ _LOG = logging.getLogger("Coconodz.startup.Katana")
 if VAR_NAME in os.environ:
     COCONODZ_PARENT = os.path.normpath(os.environ[VAR_NAME].split(PACKAGE_NAME, 1)[0].strip(";"))
     sys.path.append(COCONODZ_PARENT)
-    sys.path.append(os.path.join(COCONODZ_PARENT, PACKAGE_NAME))
-    sys.path.append(os.path.join(COCONODZ_PARENT, PACKAGE_NAME, "site-packages"))
-
-from etc.katana.nodegraph import Nodzgraph
